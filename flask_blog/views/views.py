0f4d185b7fe80e0a7598364cd3ee1bc49d74d77e
null_null <nullnullprogramming@gmail.com>
2019-07-21 14:49:12 +0900

    fix

--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -2,25 +2,36 @@
 
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from functools import wraps
 
 
-@app.route('/login', methods=['GET', 'POST'])
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        return view(*args, **kwargs)
+
+    return inner
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
 
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            flash('ユーザ名が違います')
-        elif request.form['password'] != app.config['PASSWORD']:
-            flash('パスワードが違います')
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            flash("ユーザ名が違います")
+        elif request.form["password"] != app.config["PASSWORD"]:
+            flash("パスワードが違います")
         else:
-            flash('ログインしました')
-            session['logged_in'] = True
-            return redirect(url_for('show_entries'))
-    return render_template('login.html')
+            flash("ログインしました")
+            session["logged_in"] = True
+            return redirect(url_for("show_entries"))
+    return render_template("login.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    flash('ログアウトしました')
-    session.pop('logged_in', None)
-    return redirect(url_for('show_entries'))
+    flash("ログアウトしました")
+    session.pop("logged_in", None)
+    return redirect(url_for("show_entries"))
